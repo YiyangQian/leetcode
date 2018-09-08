a237ae2f7518541ea732b4f7437769dc5f699b89
YY Qian <yy.qian@autodesk.com>
2018-09-08 17:14:14 -0400

    solution to 389

---
+++ 389.Find-the-Difference.java
@@ -0,0 +1,8 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int sum1 = 0, sum2 = 0;
+        for (int i = 0; i < s.length(); i++) sum1 += s.charAt(i) - 'a';
+        for (int i = 0; i < t.length(); i++) sum2 += t.charAt(i) - 'a';
+        return (char) ('a' + sum2 - sum1);
+    }
+}
