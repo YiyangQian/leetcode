aaa076a2e9b9522012959284f394c287cce77031
YY Qian <yy.qian@autodesk.com>
2018-09-13 16:03:20 -0400

    solution to 92

---
+++ 92.Reverse-LinkedList-2.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode cur = head;
+        for (int i = 1; i < m; i++) {
+            pre = pre.next;
+            cur = cur.next;
+        }
+        for (int i = 0; i < n - m; i++) {
+            ListNode toMove = cur.next;
+            ListNode nextOfPre = pre.next;
+            cur.next = cur.next.next;
+            pre.next = toMove;
+            toMove.next = nextOfPre;
+        }
+        return dummy.next;
+    }
+}
