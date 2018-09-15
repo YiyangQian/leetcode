9e3acbbdd90a2a0aaa799dddb1a28e616ede13bd
YY Qian <yy.qian@autodesk.com>
2018-09-15 15:52:41 -0400

    solution to 94 iteratively

---
+++ 94.Binary-Tree-Inorder-Traversal-iterative.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        List<Integer> res = new LinkedList<Integer>();
+        while (!(stack.isEmpty() && root == null)) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                root = stack.pop();
+                res.add(root.val);
+                root = root.right;
+            }
+        }
+        return res;
+    }
+}
